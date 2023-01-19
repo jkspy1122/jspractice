e37a48af667a01bbaa3a8d6a48000c4b5d2936fd
jkspytest <jkspy1122@gmail.com>
2023-01-20 04:06:45 +0800

    add price data array save to object

--- downloadApiData.js
+++ downloadApiData.js
@@ -6,19 +6,23 @@ class apiCollection {
     async getJson() {
         let exInfo = await fetch(this.api);
         exInfo = await exInfo.json();
-        console.log(exInfo);
-        return exInfo;
+        // console.log(exInfo);
+        // return exInfo;
+        let finalData = exInfo.map(d => {
+            return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+          });
+        console.log(finalData);
     }
 }
 
 const apiExchangeInfo = new apiCollection("https://fapi.binance.com/fapi/v1/exchangeInfo");
-const apiKline = new apiCollection("https://fapi.binance.com/fapi/v1/exchangeInfo");
+const apiKline = new apiCollection("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000");
 const apiInterestRate = new apiCollection("https://fapi.binan");
 
-apiExchangeInfo.getJson();
+// apiExchangeInfo.getJson();
 
 
-// apiKline.getJson();
+apiKline.getJson();
 
 
 // apiInterestRate.getJson();
